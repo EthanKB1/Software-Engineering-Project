0bc638076de68b1c81dc086e9ce867670c2e3629
Ethan <ethankyle.bannister@gmail.com>
2024-03-08 15:33:27 +0000

    PUG layout

--- Code/src/index.js
+++ Code/src/index.js
@@ -1,7 +1,23 @@
 const express = require ('express');
 const  index = express ();
+
 const port = process.env.PORT || 3000;
-index.get ('/', (req, res) => res.send('Hello World!'));
+
+//index.get ('/', (req, res) => res.send('Hello World!'));
+
+// use the PUG templating engine 
+index.set ('view engine', 'pug');
+index.set('views', './index/views'); // specify the views directory
+
+// Create a route for root
+
+index.get("/", function(req, res) {
+    res.render("index");
+});
+ 
+
+
+
 index.listen (port, () => {
     console.log('Server ready!');
     console.log('The port  is: ' + port);
