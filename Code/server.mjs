6d0645c43a33defe14b741d6ed93d0f739c51da5
Ethan <ethankyle.bannister@gmail.com>
2024-03-20 17:54:27 +0000

    index.js code moved to server.js

---
+++ Code/server.mjs
@@ -0,0 +1,22 @@
+// server.js
+
+const express = require('express');
+const index = express();
+const port = process.env.PORT || 3000;
+
+// Set up the PUG templating engine 
+index.set('view engine', 'pug');
+index.set('views', './views'); // specify the views directory
+
+// Create a route for root - /
+index.get("/", function(req, res) {
+    // Set up an array of data
+    var test_data = ['one', 'two', 'three', 'four'];
+    // Send the array through to the template as a variable called data
+    res.render("index", {'title':'My index page', 'heading':'My heading', 'data':test_data});
+});
+
+index.listen(port, () => {
+    console.log('Server ready!');
+    console.log('The port is: ' + port);
+});
