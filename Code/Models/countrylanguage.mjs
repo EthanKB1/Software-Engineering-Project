3d896bb79b38873202d8cb82e02ba940823f0972
Ethan <ethankyle.bannister@gmail.com>
2024-03-22 15:51:10 +0000

    Create countrylanguage.mjs

---
+++ Code/Models/countrylanguage.mjs
@@ -0,0 +1,15 @@
+export default class CountryLanguage {
+    countrycode;
+    language;
+    isOfficial;
+    percentage;
+
+    constructor(countrycode, language, isofficial, percentage) {
+        this.countrycode = countrycode;
+        this.language = language;
+        this.isOfficial = isofficial;
+        this.percentage = percentage;
+
+        }
+
+}
