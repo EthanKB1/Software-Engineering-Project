5e132558dea51139282565828ec12669eeca00b9
garciaca1 <158593060+garciaca1@users.noreply.github.com>
2024-04-23 16:48:32 +0100

    update server.mjs

--- Code/src/server.mjs
+++ Code/src/server.mjs
@@ -1,8 +1,8 @@
 import express from "express";
 import mysql from "mysql2/promise";
 import DatabaseService from "./services/database.services.mjs";
+import { User } from "./models/user";
 
-const { User } = require("./models/user");
 const app = express();
 const port = process.env.PORT || 3000;
 
